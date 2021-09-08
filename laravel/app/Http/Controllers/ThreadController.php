85496b285016393e781d2cd7308eb9d87a0efbdf
yumekiti <yumekiti120440@gmail.com>
2021-09-09 01:10:56 +0900

    add: Thread

--- laravel/app/Http/Controllers/ThreadController.php
+++ laravel/app/Http/Controllers/ThreadController.php
@@ -5,15 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Thread;
+use App\User;
 
 class ThreadController extends Controller
 {
     //
     public function index(){
         $threads = Thread::all();
+        $users = User::all();
 
         $params = [
             'threads' => $threads,
+            'users' => $users,
         ];
 
         return view('index', $params);
--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -1,11 +1,33 @@
-@foreach ($threads as $thread)
-    <p>{{ $thread }}</p>
-@endforeach
+@extends('layouts.app')
 
-<hr>
+@section('content')
 
-<pre>
-@php
-    var_dump($threads);
-@endphp
-</pre>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Thread list</div>
+
+                <div class="card-body">
+                    @foreach ($threads as $thread)
+                    <a href="/thread/{{$thread->id}}" style="text-decoration: none; color: black;">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text">{{ $users[($thread->user_id - 1)]->name }}</p>
+                                <h5 class="card-title">{{ $thread->title }}</h5>
+                            </div>
+                            <div class="card-footer text-muted text-right">
+                                {{ $thread->updated_at }}
+                            </div>
+                        </div>
+                        <br>
+                    </a>
+                    @endforeach
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
